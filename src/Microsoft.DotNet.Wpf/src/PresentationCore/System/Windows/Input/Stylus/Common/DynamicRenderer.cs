cd576c2d6b0aa1e0409d4019012da209d68400fc
hjcgqj <84453680+hjcgqj@users.noreply.github.com>
2021-07-14 18:28:05 +0800

    Update DynamicRenderer.cs

--- src/Microsoft.DotNet.Wpf/src/PresentationCore/System/Windows/Input/Stylus/Common/DynamicRenderer.cs
+++ src/Microsoft.DotNet.Wpf/src/PresentationCore/System/Windows/Input/Stylus/Common/DynamicRenderer.cs
@@ -997,7 +997,7 @@ namespace System.Windows.Input.StylusPlugIns
                 {
                     if(_multiStrokeInfoDic[strokeId].Count > 0)
                     {
-                        StrokeInfo si = _multiStrokeInfoDic[strokeId].Value[0];
+                        StrokeInfo si = _multiStrokeInfoDic[strokeId][0];
                         if (_multiStrokeInfoDic.ContainsKey(si.StylusId))
                         {
                             _multiStrokeInfoDic.Remove(si.StylusId);

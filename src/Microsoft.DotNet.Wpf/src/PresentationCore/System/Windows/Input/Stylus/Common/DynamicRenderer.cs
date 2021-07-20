017696930df23aa1db1c3858f7c919ad5b3786bc
hjcgqj <84453680+hjcgqj@users.noreply.github.com>
2021-07-20 10:21:05 +0800

    Update DynamicRenderer.cs

--- src/Microsoft.DotNet.Wpf/src/PresentationCore/System/Windows/Input/Stylus/Common/DynamicRenderer.cs
+++ src/Microsoft.DotNet.Wpf/src/PresentationCore/System/Windows/Input/Stylus/Common/DynamicRenderer.cs
@@ -537,12 +537,16 @@ namespace System.Windows.Input.StylusPlugIns
                     si._allPoints.Add(rawStylusInput.GetStylusPoints());
                     if (null != _applicationDispatcher)
                     {
+                        ContainerVisual rootVisualTmp = ((ContainerVisual)si.StrokeHV.VisualTarget.RootVisual);
+                        ContainerVisual strokeVisualTmp = si.StrokeRTICV;
+
                         StylusPointCollection strokePoints = new StylusPointCollection(si._allPoints);
                         _applicationDispatcher.BeginInvoke(new Action(() =>
                         {
                             //TransitionStrokeVisuals(si, false);
                             stylusUpProcess(strokePoints);
-                            ((ContainerVisual)si.StrokeHV.VisualTarget.RootVisual).Children.Remove(si.StrokeRTICV);
+                            rootVisualTmp.Children.Remove(strokeVisualTmp);
+                            //((ContainerVisual)si.StrokeHV.VisualTarget.RootVisual).Children.Remove(si.StrokeRTICV);
                         }));
                     }
                     rawStylusInput.NotifyWhenProcessed(si);

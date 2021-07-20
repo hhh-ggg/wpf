b8f6eedaa9be0ba2660c4306dabf842f1d07949e
hjcgqj <84453680+hjcgqj@users.noreply.github.com>
2021-07-20 11:02:37 +0800

    Update DynamicRenderer.cs

--- src/Microsoft.DotNet.Wpf/src/PresentationCore/System/Windows/Input/Stylus/Common/DynamicRenderer.cs
+++ src/Microsoft.DotNet.Wpf/src/PresentationCore/System/Windows/Input/Stylus/Common/DynamicRenderer.cs
@@ -540,9 +540,9 @@ namespace System.Windows.Input.StylusPlugIns
                         StylusPointCollection strokePoints = new StylusPointCollection(si._allPoints);
                         _applicationDispatcher.BeginInvoke(new Action(() =>
                         {
-                            //TransitionStrokeVisuals(si, false);
+                            TransitionStrokeVisuals(si, false);
                             stylusUpProcess(strokePoints);
-                            removeSiInfo(si);
+                            //removeSiInfo(si);
                         }));
 
                     }
